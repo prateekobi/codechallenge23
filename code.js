fd342e0b1144732d08dfed0d55ab0aed4970c5e9
Prateek Obireddy <prateekobireddy@192-168-1-103.tpgi.com.au>
2019-11-20 20:43:21 +1100

    Cleaned up code

--- code.js
+++ code.js
@@ -1,21 +1,17 @@
+
 function myFunction() {
   let price = document.getElementById('price').value;
   let tip = '';
+  console.log('PRICE: ', price);
   if (!price) {
-    document.getElementById('tip').innerHTML = 'Please enter an amount';
-    return false;
+    tip = 'Please enter an amount';
   } else if (price <= 50) {
-    tip = (price * 20) /100
-    document.getElementById('tip').innerHTML = tip;
-    return false;
+    tip = (price * 20) /100;
   } else if (price > 50 && price < 200 ) {
     tip = (price * 15) / 100;
-    document.getElementById('tip').innerHTML = tip;
-    return false;
   } else if (price >= 200) {
     tip = (price * 10) / 100;
-    document.getElementById('tip').innerHTML = tip;
-    return false;
   }
+  document.getElementById('tip').innerHTML = tip;
   return false;
 };
