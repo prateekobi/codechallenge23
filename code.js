f9920d782496ac7d282576f049b38abaa87cee12
Prateek Obiredddy <prateekobi@gmail.com>
2019-11-25 20:01:49 +1100

    updated

--- code.js
+++ code.js
@@ -1,17 +1,36 @@
+// var a = 124;
+// var b = 48;
+// var c = 258;
 
-function myFunction() {
-  let price = document.getElementById('price').value;
-  let tip = '';
-  console.log('PRICE: ', price);
-  if (!price) {
-    tip = 'Please enter an amount';
-  } else if (price <= 50) {
-    tip = (price * 20) /100;
-  } else if (price > 50 && price < 200 ) {
-    tip = (price * 15) / 100;
-  } else if (price >= 200) {
-    tip = (price * 10) / 100;
+function tipArrCalculator(a, b, c) {
+  var arr = [a, b, c];
+  var tipArr = [];
+  var totalArr = [];
+
+  var obj = {};
+
+  for (var i = 0; i < arr.length; i++) {
+    var tip = 0;
+    var total = 0;
+
+    if(arr[i] <= 50) {
+      tip = arr[i] * 20/100;
+      total = tip + arr[i];
+      tipArr.push(tip);
+      totalArr.push(total);
+    } else if (arr[i] > 50 && arr[i] <= 200) {
+      tip = arr[i] * 15/100;
+      total = tip + arr[i];
+      tipArr.push(tip);
+      totalArr.push(total);
+    } else if (arr[i] > 200) {
+      tip = arr[i] * 10/100;
+      total = tip + arr[i];
+      tipArr.push(tip);
+      totalArr.push(total);
+    }
   }
-  document.getElementById('tip').innerHTML = tip;
-  return false;
-};
+  obj.tip = tipArr;
+  obj.total = totalArr;
+  return obj;
+}
